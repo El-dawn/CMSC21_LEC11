3f2a268947d8eb4e437858ddd4fc490376e4237c
Eldon_ <130384980+El-dawn@users.noreply.github.com>
2023-06-02 15:49:53 +0800

    Add files via upload

---
+++ Task2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h> /* bool */
+#include <ctype.h> /* toupper, isaplha */
+
+// Keeps track of how many times each letter occurs in each word
+void scan_word(int* occurences){
+	char c;
+	while ((c = getchar()) != '\n') {
+		if (isalpha(c)){
+			occurences[toupper(c) - 'A']++;
+		}
+	}
+}
+
+// Checks if both occurence arrays are equal
+bool is_anagram(int* occurences1, int* occurences2){
+	for (int i = 0; i < 26; i++) {
+		if (occurences1[i] != occurences2[i]) {
+			printf("The words are not anagrams.\n");
+			return false;
+		}
+	}
+	printf("The words are anagrams.\n");
+	return true;
+}
+
+int main(void) {
+	
+	int occurences1[26] = {0}, occurences2[26]= {0};
+	bool same;
+
+	printf("Enter first word: ");
+	scan_word(occurences1);
+	printf("Enter second word: ");
+	scan_word(occurences2);
+
+	same = is_anagram(occurences1, occurences2);
+	return 0;
+}
